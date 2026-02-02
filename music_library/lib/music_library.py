ced1694efb3b9e738d4daf68136bb7445f86fb38
mahmudur mahid <m.mahid@icloud.com>
2026-02-02 08:58:05 +0000

    removed excess whitespace and comments

--- music_library/lib/music_library.py
+++ music_library/lib/music_library.py
@@ -1,24 +1,13 @@
 class MusicLibrary:
-    # Public properties:
-    #   tracks: a list of strings representing tracks
-
     def __init__(self):
         self.tracks = []
 
     def add(self, track):
-        # Parameters:
-        #   track: an instance of Track
-        # Returns:
-        #   Nothing
         self.tracks.append(track)
 
     def search_by_title(self, keyword):
-        # Parameters:
-        #   keyword: a string
-        # Returns:
-        #   a list of Track instances with titles that include the keyword
         matches = []
-        
+
         for song in self.tracks:
             if keyword in song.title:
                 matches.append(song)
