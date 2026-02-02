df9c7662122ef0d512469a36f2226d1541515f4b
mahmudur mahid <m.mahid@icloud.com>
2026-02-02 08:56:36 +0000

    completed logic for music library class

--- music_library/lib/music_library.py
+++ music_library/lib/music_library.py
@@ -3,18 +3,23 @@ class MusicLibrary:
     #   tracks: a list of strings representing tracks
 
     def __init__(self):
-        pass
+        self.tracks = []
 
     def add(self, track):
         # Parameters:
         #   track: an instance of Track
         # Returns:
         #   Nothing
-        pass
+        self.tracks.append(track)
 
     def search_by_title(self, keyword):
         # Parameters:
         #   keyword: a string
         # Returns:
         #   a list of Track instances with titles that include the keyword
-        pass
+        matches = []
+        
+        for song in self.tracks:
+            if keyword in song.title:
+                matches.append(song)
+        return matches
