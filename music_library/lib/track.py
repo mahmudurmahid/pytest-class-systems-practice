2a9636bbe23638c71564f75dabebf4071104160d
mahmudur mahid <m.mahid@icloud.com>
2026-02-02 08:57:35 +0000

    removed comments and excess whitespace

--- music_library/lib/track.py
+++ music_library/lib/track.py
@@ -1,18 +1,7 @@
 class Track:
-    # Public properties:
-    #   title: a string
-    #   artist: a string
-
     def __init__(self, title, artist):
-        # Parameters:
-        #   title: a string
-        #   artist: a string
-        # Side-effects:
-        #   Sets the title and artist properties
         self.title = title
         self.artist = artist
 
     def format(self):
-        # Returns:
-        #   a string in the format "TITLE by ARTIST"
         return f"{self.title} by {self.artist}"
